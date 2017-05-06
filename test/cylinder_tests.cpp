2f191356b8b7a2742ffa8dae1a7a838d9b9a5f71
Dillon Huff <dillonhuff@gmail.com>
2017-05-06 14:45:23 -0700

    Added a positive test of 2D circle containment

--- test/cylinder_tests.cpp
+++ test/cylinder_tests.cpp
@@ -4,13 +4,23 @@
 
 namespace sot {
 
-  TEST_CASE("Circle containment") {
-    vec_3 pt(2.0, 0.0, 0.0);
+  TEST_CASE("2D circle containment") {
 
     vec_3 center(1.0, 0.0, 0.0);
     double radius(0.25);
 
-    REQUIRE( !in_circle_2D( pt, center, radius ) );
+    SECTION("Point outside circle") {
+      vec_3 pt(2.0, 0.0, 0.0);
+
+      REQUIRE( !in_circle_2D( pt, center, radius ) );
+    }
+
+    SECTION("Point inside circle") {
+      vec_3 pt(1.1, -0.05, 10.0);
+
+      REQUIRE( in_circle_2D( pt, center, radius ) );
+    }
+
   }
 
 }
